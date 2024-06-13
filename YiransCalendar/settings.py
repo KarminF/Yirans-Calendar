106cd9b3d7e9106e046e0d2f23212372c6ab6f5b
karmin <ddddr6@163.com>
2024-06-13 17:13:09 +0200

    changed css

--- YiransCalendar/settings.py
+++ YiransCalendar/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'yiransCalendar',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': '99907566',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
